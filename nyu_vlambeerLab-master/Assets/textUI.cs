b79ab354232de57b9381a752b3b3836eef5d37bd
Yifan <ym2159@nyu.edu>
2020-10-25 13:48:02 -0700

    cityGenerator

---
+++ nyu_vlambeerLab-master/Assets/textUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textUI : MonoBehaviour
+{
+    Text instruction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instruction = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        instruction.text = "City.1.0, Press R to reset";
+    }
+}
